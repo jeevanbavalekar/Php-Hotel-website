2f05e99e29c4de8458e90b73b9cf50d44acb2845
jeevanbavalekar <81096139+jeevanbavalekar@users.noreply.github.com>
2022-08-19 18:12:02 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">Khushi</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+      <a href="login.php">Login/Signup</a>
+
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-1.png) no-repeat">
+   <h1>about us</h1>
+</div>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="image">
+      <img src="image/2.jpg" alt="">
+      <img src="image/1.jpg" alt="">
+      <img src="image/5.jpg" alt="">
+
+   </div>
+
+   <div class="content">
+      <h3>why choose us?</h3>
+      <br>
+      <h1>Room Allotment</h1>
+      <p>Room Allotment Will Be Done At Resort, Room Charges As Per Applicable Sales Tariff.</p>
+      <p>For Check-In Before Our Standard Check- In Time Room Will Be Allotted Only If Available.</p>
+      <p> All Arrivals Prior To 06.00 Am Are Held From The Previous Evening And Billed For From The Previous Evening. All Arrivals Between 6.00am To 9.00 Am Will be Charged Half The Tariff On Room Only Basis. </p><br>
+      <h1>Mandatory</h1>
+      <p>
+Govt. recognised Photo ID with Address Proof is Compulsory (Aadhar Card/Pan Card).We do not allow Single Person in a Room (with Group are Allowed)
+</p> <br>
+<h1>Cab/Taxi service</h1>
+<p>   Our Front Desk Representative Will Help You With The Local Sightseeing Points & Can Arrange For Tourist Taxi. But You Have To Inform 30 Min Before For     A TAXI/ CAB (Charges To Be Paid Directly By Guest). </p>
+      <div class="icons-container">
+         <div class="icons">
+            <i class="fas fa-map"></i>
+            <span>nearby places</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>affordable price</span>
+         </div>
+         <div class="icons">
+            <i class="fas fa-headset"></i>
+            <span>24/7 service</span>
+         </div>
+      </div>
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- reviews section starts  -->
+
+<section class="reviews">
+
+   <h1 class="heading-title"> hotel reviews </h1>
+
+   <div class="swiper reviews-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus facilis laudantium magnam saepe magni ullam possimus laborum voluptas, veniam ipsum officiis quae doloribus minima ut quis. Molestiae qui distinctio possimus?</p>
+            <h3>john deo</h3>
+            <span>traveler</span>
+            <img src="images/pic-1.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus repellendus et suscipit optio similique totam aut laborum ullam sequi! In!</p>
+            <h3>john deo</h3>
+            <img src="images/pic-2.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim deserunt, ab at ea iste odio placeat doloribus eum dolores tempore obcaecati, dolore esse aperiam. Excepturi ad quo distinctio, dignissimos voluptate, dolores, saepe animi eum atque sint esse ab exercitationem!</p>
+            <h3>john deo</h3>
+            <img src="images/pic-3.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus recusandae cum quibusdam odio dignissimos nemo?</p>
+            <h3>john deo</h3>
+            <img src="images/pic-4.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus facilis laudantium magnam saepe magni ullam possimus laborum voluptas, veniam ipsum officiis quae doloribus minima ut quis. Molestiae qui distinctio possimus?</p>
+            <h3>john deo</h3>
+            <img src="images/pic-5.png" alt="">
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="stars">
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+               <i class="fas fa-star"></i>
+            </div>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus facilis laudantium magnam saepe magni ullam possimus laborum voluptas, veniam ipsum officiis quae doloribus minima ut quis. Molestiae qui distinctio possimus?</p>
+            <h3>john deo</h3>
+            <img src="images/pic-6.png" alt="">
+         </div>
+
+      </div>
+
+   </div>
+
+</section>
+
+<!-- reviews section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+      <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +91-9420771488  </a>
+         <a href="#"> <i class="fas fa-phone"></i> +91-9420771488 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> reservations@khushi.in  </a>
+         <a href="#"> <i class="fas fa-map"></i> Mahabaleshwar, Satara 412806 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/login/"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://www.instagram.com/accounts/login/"> <i class="fab fa-instagram"></i> instagram </a>
+      </div>
+
+   </div>
+
+   <div class="credit">  © KHUSHI Riverside Retreat Pvt Ltd.  | Terms & Conditions <br>
+    Designed & Developed by <span>Vaishnavi Bavalekar & Mayuresh Shirsath </span> </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+   <!-- header section starts  -->
+
+   <section class="header">
+
+      <a href="home.php" class="logo">Khushi</a>
+
+      <nav class="navbar">
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="package.php">package</a>
+         <a href="book.php">book</a>
+         <a href="login.php">Login/Signup</a>
+
+      </nav>
+
+      <div id="menu-btn" class="fas fa-bars"></div>
+
+   </section>
+
+   <!-- header section ends -->
+
+   <!-- home section starts  -->
+
+   <section class="home">
+
+      <div class="swiper home-slider">
+
+         <div class="swiper-wrapper">
+
+            <div class="swiper-slide slide" style="background:url(image/2.jpg) no-repeat">
+               <div class="content">
+                  <span>book, stay, travel, explore</span>
+                  <h3>Experience a good stay, enjoy fantastic offers</h3>
+                  <a href="package.php" class="btn">discover more</a>
+               </div>
+            </div>
+
+            <div class="swiper-slide slide" style="background:url(image/superiordeluxeroom1.jpg) no-repeat">
+               <div class="content">
+                  <span>book, stay, travel, explore</span>
+                  <h3>Happy days are here again! </h3>
+                  <a href="package.php" class="btn">discover more</a>
+               </div>
+            </div>
+
+            <div class="swiper-slide slide" style="background:url(image/3.jpg) no-repeat">
+               <div class="content">
+                  <span>book, stay, travel, explore</span>
+                  <h3>make your stay worthwhile</h3>
+                  <a href="package.php" class="btn">discover more</a>
+               </div>
+            </div>
+
+         </div>
+
+         <div class="swiper-button-next"></div>
+         <div class="swiper-button-prev"></div>
+
+      </div>
+
+   </section>
+
+   <!-- home section ends -->
+
+   <!-- services section starts  -->
+
+   <section class="services">
+
+      <h1 class="heading-title"> our services </h1>
+      <div class="box-container">
+
+         <div class="box">
+            <img src="image/taxi.png" alt="">
+            <h3>cab facilities</h3>
+         </div>
+
+         <div class="box">
+            <img src="image/swimming.png" alt="">
+            <h3>swamming pool</h3>
+         </div>
+
+         <div class="box">
+            <img src="image/bed.png" alt="">
+            <h3>master bedrooms</h3>
+         </div>
+
+         <div class="box">
+            <img src="image/dish.png" alt="">
+            <h3>restaurant</h3>
+         </div>
+
+         <div class="box">
+            <img src="image/icon1.png" alt="">
+            <h3>wifi coverage</h3>
+         </div>
+
+      </div>
+
+   </section>
+
+   <!-- services section ends -->
+
+   <!-- home about section starts  -->
+
+   <section class="home-about">
+
+      <div class="image">
+         <img src="image/IMG-20201018-WA0035.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>about us</h3>
+         <p>Our KHUSHI RIVER SIDE RETREAT, spread across 2 acres with spectacular views of Venna River & lush greenery,
+            is located at the highest altitude in Mahabaleshwar.We combine elegance with untamed natural beauty and
+            offer luxurious accommodation,to provide you the most pleasant experience in this hill station.</p>
+         <a href="about.php" class="btn">read more</a>
+      </div>
+
+   </section>
+
+   <!-- home about section ends -->
+
+   <!-- home packages section starts  -->
+
+   <section class="home-packages">
+
+      <h1 class="heading-title"> our rooms </h1>
+
+      <div class="box-container">
+
+         <div class="box">
+            <div class="image">
+               <img src="image/deluxeroom.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Deluxe Double Room</h3>
+               <p>Get just ₹ 999</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+
+         <div class="box">
+            <div class="image">
+               <img src="image/superiordeluxeroom.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Superior Double Room</h3>
+               <p>Get just ₹ 1499</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+
+         <div class="box">
+            <div class="image">
+               <img src="image/forestview.jpg" alt="">
+            </div>
+            <div class="content">
+               <h3>Forest View Room</h3>
+               <p>Get just ₹ 1299</p>
+               <a href="book.php" class="btn">book now</a>
+            </div>
+         </div>
+
+
+
+      </div>
+
+   </section>
+
+   <!-- home packages section ends -->
+
+   <!-- home offer section starts  -->
+
+   <section class="home-offer">
+      <div class="content">
+         <h3>upto 20% off</h3>
+         <p> A clean bed for a tired soul.</p>
+         <p>Luxury is not a place, it's an experience.
+         </p>
+         <p>Located at the perfect place for a perfect vacation.
+         </p>
+         <p>Always deliver more than expected.
+         </p>
+         <a href="book.php" class="btn">book now</a>
+      </div>
+   </section>
+
+   <!-- home offer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <!-- footer section starts  -->
+
+   <section class="footer">
+
+      <div class="box-container">
+
+         <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+            <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+            <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+            <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+         </div>
+
+         <div class="box">
+            <h3>extra links</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+         </div>
+
+         <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +91-9420771488 </a>
+            <a href="#"> <i class="fas fa-phone"></i> +91-9420771488 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> reservations@khushi.in </a>
+            <a href="#"> <i class="fas fa-map"></i> Mahabaleshwar, Satara 412806 </a>
+         </div>
+
+         <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/login/"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="https://www.instagram.com/accounts/login/"> <i class="fab fa-instagram"></i> instagram </a>
+         </div>
+
+      </div>
+
+      <div class="credit"> © KHUSHI Riverside Retreat Pvt Ltd. | Terms & Conditions <br>
+         Designed & Developed by <span>Vaishnavi Bavalekar & Mayuresh Shirsath </span> </div>
+
+   </section>
+
+   <!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+   <!-- swiper js link  -->
+   <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+   <!-- custom js file link  -->
+   <script src="js/script.js"></script>
+
+</body>
+
+</html>
